04f83c485a81bf26ca1983074998e05e5a03aa17
brushiol <128639480+brushiol@users.noreply.github.com>
2025-06-09 23:01:03 -0700

    why are you reading this

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let rw = document.getElementById("switch");
 let ficont = document.getElementById("ficont");
 let finp = document.getElementById("finp");
 let latest = "0.3"
-function rwclicked() {
+/*document.addEventListener("DOMContentLoaded", function () {
+    document.documentElement.style.fontFamily = 'Comic Sans MS';
+});*/
+rw.addEventListener("click", function () {
     console.log(true)
     let chosen = rw.checked;
     toggleAttribute(idtext, "hidden", chosen)
@@ -18,7 +21,7 @@ function rwclicked() {
     } else {
  
     }*/
-}
+});
 
 function toggleAttribute(elt, name, bool) {
     if (bool) {
